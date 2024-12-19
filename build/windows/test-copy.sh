72fe3d48fdef5976d8a534a395ae4effd677099a
Ilya Putilin <putilya16@gmail.com>
2024-12-19 15:00:22 +0700

    update test script

--- build/windows/test-copy.sh
+++ build/windows/test-copy.sh
@@ -14,18 +14,18 @@
 #
 # Prepare folders structure
 #
-#EXE_APP_DIR="files_64"
-#mkdir $EXE_APP_DIR
+EXE_APP_DIR="files_64"
+mkdir $EXE_APP_DIR
 #
 # Copy files for MSIX packaging
 #
 #cd $EXE_APP_DIR
-#mkdir Images
-#ROOT_DIR="../../../"
-#cp $ROOT_DIR/Images/* $EXE_APP_DIR/Images
-#cp $ROOT_DIR/AppxManifest.xml $EXE_APP_DIR
+mkdir $EXE_APP_DIR/Images
+#ROOT_DIR="../../"
+cp $ROOT_DIR/Images/* $EXE_APP_DIR/Images
+cp $ROOT_DIR/AppxManifest.xml $EXE_APP_DIR
 
-mkdir files_64
-mkdir files_64/Images
-cp ../../Images/* files_64/Images
-cp ../../AppxManifest.xml files_64
+#mkdir files_64
+#mkdir files_64/Images
+#cp ../../Images/* files_64/Images
+#cp ../../AppxManifest.xml files_64
