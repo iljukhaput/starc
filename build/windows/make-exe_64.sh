9ccd932923184086fef71d7b2ea42870add6723b
Ilya Putilin <81326673+iljukhaput@users.noreply.github.com>
2024-12-19 19:51:02 +0700

    Update make-exe_64.sh

--- build/windows/make-exe_64.sh
+++ build/windows/make-exe_64.sh
@@ -86,7 +86,7 @@ cp "$VCToolsRedistDir/x64/Microsoft.VC143.CRT/vcruntime140_1.dll" $EXE_APP_DIR
 #
 # Copy files for MSIX packaging
 #
-ROOT_DIR="../../"
+ROOT_DIR="../.."
 ls -l $ROOT_DIR
 mkdir $EXE_APP_DIR/Images
 cp $ROOT_DIR/Images/StoreLogo.scale-400.png $EXE_APP_DIR/Images
